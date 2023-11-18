d93c1cce40eeff50edc4596e5fe005279544d042
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-11-18 19:52:27 +0100

    Mostrar el tablero hecho

---
+++ src/mostrar_tablero.py
@@ -0,0 +1,23 @@
+from src.generacion_tablero import generar_tablero
+
+def mostrar_tablero(tablero_a_mostrar:list)->str:
+
+    """
+    Función que se encarga de imprimir en condiciones el contenido del tablero
+    Parameters
+    ----------
+    tablero_a_mostrar
+        El tablero que se va a mostrar
+
+    Returns
+    -------
+        Devuelve la string adecuada para mostrarlo bien a la hora de imprimir
+
+    """
+    mensaje_tablero = ""
+    for fila in tablero_a_mostrar:
+        for celda in fila:
+            mensaje_tablero += celda + " "
+        mensaje_tablero += "\n"
+    return mensaje_tablero
+
